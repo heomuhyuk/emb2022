8fc7b8a04b364199a1938ecbc5156d2f0a0fb3be
parkmoonsoooo <116923639+parkmoonsoooo@users.noreply.github.com>
2022-10-30 19:27:13 +0900

    박문수 수정

--- 2.c
+++ 2.c
@@ -1,6 +1,6 @@
 #include "myProject.h"
-int printMyName3(void) //1~2중 하나. 파일이름에 맞게 할 것
+int printMyName2(void) //1~2중 하나. 파일이름에 맞게 할 것
 {
-printf ("3. my name is 박문수\n"); //자기 이름
+printf ("2. my name is 박문수\n"); //자기 이름
 return 1;
 }
