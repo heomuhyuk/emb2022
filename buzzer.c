0edde8e0ff70def2d3029fd221363af67105f265
hmh3913 <hmh3913@naver.com>
2022-11-28 10:28:30 +0900

    buzzer
    부저 완성본

--- buzzer.c
+++ buzzer.c
@@ -5,68 +5,76 @@
 #include <unistd.h>
 #include <dirent.h>
 #include "buzzer.h"
-#include <sys/ioctl.h>
-#include <ctype.h>
-#include <sys/ipc.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 
 #define MAX_SCALE_STEP 8
 #define BUZZER_BASE_SYS_PATH "/sys/bus/platform/devices/"
 #define BUZZER_FILENAME "peribuzzer"
 #define BUZZER_ENABLE_NAME "enable"
 #define BUZZER_FREQUENCY_NAME "frequency"
-const int musicScale[MAX_SCALE_STEP] =
+static const int musicScale[MAX_SCALE_STEP] =
 {
-262, /*do*/ 294,330,349,392,440,494, /* si */ 523
+    262, /*do*/ 294,330,349,392,440,494, /* si */ 523
 };
-char gBuzzerBaseSysDir[128]; //sys/bus/platform/devices/peribuzzer.XX 가 결정됨
-static int fd;
 
-int buzzerInit(void)
+static char gBuzzerBaseSysDir[128]; ///sys/bus/platform/devices/peribuzzer.XX 가 결정됨
+static int fd_en, fd_freq;
+static char path_en[200] = {0,}, path_freq[200] = {0,};
+
+int findBuzzerSysPath(char *gBuzzerBaseSysDir)     //버저 경로 찾기: /sys/bus/platform/devices/peribuzzer.XX 의 XX를 결정하는 것
 {
     DIR * dir_info = opendir(BUZZER_BASE_SYS_PATH);
     int ifNotFound = 1;
-    if (dir_info != NULL){
-    	while (1){
-    	struct dirent *dir_entry;
-    	dir_entry = readdir (dir_info);
-    	if (dir_entry == NULL) break;
-    	if (strncasecmp(BUZZER_FILENAME, dir_entry->d_name, strlen(BUZZER_FILENAME)) == 0){
-        	ifNotFound = 0;
-        	sprintf(gBuzzerBaseSysDir,"%s%s/",BUZZER_BASE_SYS_PATH,dir_entry->d_name);
-        	}
-    	}
-	}
-	printf("find %s\n",gBuzzerBaseSysDir);
-	char path[200];
-	sprintf(path,"%s%s",gBuzzerBaseSysDir, BUZZER_ENABLE_NAME);
-	int fd=open(path,O_WRONLY);
-    if ( fd < 0 )
+    if (dir_info != NULL)
     {
-        perror("driver (//dev//cnled) open error.\n");
-        return 1;
+        while (1){
+            struct dirent *dir_entry;   //XX 숫자 담을 변수
+            dir_entry = readdir (dir_info);     //XX 숫자를 변수에 넣는다
+            if (dir_entry == NULL) break;   //NULL값들어오면 break
+            if (strncasecmp(BUZZER_FILENAME, dir_entry->d_name, strlen(BUZZER_FILENAME)) == 0)
+            {
+                ifNotFound = 0;
+                sprintf(gBuzzerBaseSysDir,"%s%s/",BUZZER_BASE_SYS_PATH,dir_entry->d_name);
+            }
+        }
     }
-	return ifNotFound;
+    printf("find %s\n",gBuzzerBaseSysDir);
+    return ifNotFound;
+}
+
+int buzzerInit(void)
+{    
+    if (findBuzzerSysPath(gBuzzerBaseSysDir) == 1)
+    {
+        printf("Failed. File Not EXIT\r\n");
+        return 0;
+    }
+    sprintf(path_en, "%s%s", gBuzzerBaseSysDir, BUZZER_ENABLE_NAME);
+    sprintf(path_freq, "%s%s", gBuzzerBaseSysDir, BUZZER_FREQUENCY_NAME);
+
+    fd_en = open (path_en, O_WRONLY);
+    fd_freq = open (path_freq, O_WRONLY);
+    return 1;
 }
 
 int buzzerPlaySong(int scale)
 {
-	char path[200];
-	sprintf(path,"%s%s",gBuzzerBaseSysDir,BUZZER_ENABLE_NAME);
-    dprintf(fd, "%d", scale);
-	write(fd, &"1", 1);
+    dprintf(fd_freq, "%d", scale);
+    write(fd_en, &"1", 1);
+    
+    return 1;
 }
 
 int buzzerStopSong(void)
 {
-	char path[200];
-	sprintf(path,"%s%s",gBuzzerBaseSysDir,BUZZER_ENABLE_NAME);
-	int fd=open(path,O_WRONLY);
-	write(fd, &"0", 1);
+    write(fd_en, &"0", 1);
+
+    return 1;
 }
 
 int buzzerExit(void)
 {
-	close(fd);
+    buzzerStopSong();
+    close(fd_freq);
+    close(fd_en);
+    return 0;
 }
--- buzzertest.c
+++ buzzertest.c
@@ -5,19 +5,32 @@
 #include <unistd.h>
 #include <dirent.h>
 #include "buzzer.h"
-#include <sys/ioctl.h>
-#include <ctype.h>
-#include <sys/ipc.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+
+#define MAX_SCALE_STEP 8
+static const int musicScale[MAX_SCALE_STEP] =
+{
+    262, /*do*/ 294,330,349,392,440,494, /* si */ 523
+};
 
 int main(void)
 {
     buzzerInit();
-    buzzerPlaySong(1);
+    printf("buzzer turn on\n");
+    printf("do[1] = 262\nre[2] = 294\n,mi[3] = 330\n,pa[4] = 349\n,sol[5] = 392\n,ra[6] = 440\n,si[7] = 494\n");
+    buzzerPlaySong(musicScale[1]);
+    printf("scale : %d\n", musicScale[1]);
+    sleep(1);
+
+    buzzerPlaySong(musicScale[3]);
+    printf("scale : %d\n", musicScale[3]);
+    sleep(1);
+
+    buzzerPlaySong(musicScale[5]);
+    printf("scale : %d\n", musicScale[5]);
+    sleep(1);
+
     buzzerStopSong();
-    buzzerPlaySong(2);
-    buzzerStopSong();
+    printf("turn off\n");
     buzzerExit();
     return 0;
 }
