8da42dc1b574e26081f810eb76599660458ff4f8
hmh3913 <hmh3913@naver.com>
2022-12-01 21:29:28 +0900

    skeleton main
    기본

--- main.c
+++ main.c
@@ -1,18 +1,123 @@
-/*임베디드 시스템 2022 첫 번째 팀과제 입니다.
-조장의 main.c 코드입니다. 조원들은 1.c 2.c 3.c 코드 생성하여 만들어주세요.
-생성 코드는 ppt 숙제설명에 올라와 있습니다.
-번호는 이름 가나다 순으로 맡아 작성해주시면 감사하겠습니다.
-*/
-
 #include <stdio.h>
-#include "myProject.h"
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <ctype.h>
+#include <sys/msg.h>
+#include <sys/mman.h>
+#include <linux/input.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <pthread.h>
+#include "Temperature.h"
+#include "accelMagGyro.h"
+#include "bitmap.h"
+#include "button.h"
+#include "buzzer.h"
+#include "colorled.h"
+#include "fnd.h"
+#include "led.h"
+#include "textlcd.h"
 
-int main(void)
+    /*LED*/
+//ledLibInit();
+//ledOnOff(1,1);
+//ledOnOff(0,0);
+//ledLibExit();
+    /*온도*/
+//temp_init();
+//temp_exit();
+//temp_read()
+    /*가속도*/
+//accelInit()
+//magInit()
+//gyroInit()
+//accelRead();
+//magRead();
+//gyroRead();
+
+    /*비트맵*/
+//bitmainfunc("test.bmp");
+
+    /*버튼*/
+//buttonLibInit();
+/*	메인문 안에서
+    while(1)
+	{
+		int returnValue = 0 ;
+		returnValue = msgrcv(msgID, &msgRx, sizeof(int), 0 ,0);
+			printf("EV_KEY(");
+			switch(msgRx.keyInput)
+			{
+				case KEY_VOLUMEUP: printf("Volume up key):"); break;
+				case KEY_HOME: printf("Home key):"); break;
+				case KEY_SEARCH: printf("Search key):"); break;
+				case KEY_BACK: printf("Back key):"); break;
+				case KEY_MENU: printf("Menu key):"); break;
+				case KEY_VOLUMEDOWN:printf("Volume down key):");break;
+			}
+			printf("pressed\n");
+	}
+*/
+//buttonLibExit();
+
+    /*부저*/
+//buzzerInit();
+//buzzerPlaySong(musicScale[1]);
+//buzzerStopSong();
+//buzzerExit();
+
+
+    /*컬러엘이디*/
+//pwmLedInit();
+//pwmSetPercent(30, 0); //r    ( duty = (100- percent) )
+//sleep(1);
+//pwmInactiveAll(); 종료
+    
+    //fnd//
+//fndDisp(number , 0);
+//fndOff(void);
+
+    /*text lcd*/
+//textLCD_Init();
+//writeLCD(line_number, str);
+//textLCD_off();
+
+void AllDeviceInit()
 {
-    printf("cheif name is [허무혁]\r\n");
-    /*이 주석은 조원의 코드가 모두 push되면 해제 할것*/
-    printMyName1();
-    printMyName2();
-    printMyName3();
-    return 1;
+    //여기에 드라이브 모두 이닛
+}
+
+void AllDeviceClose()
+{
+    //여기에 드라이브 모두 클로즈
+}
+
+
+int main(void){
+	struct input_event stEvent;
+	BUTTON_MSG_T msgRx;
+	int msgID = msgget ((key_t)MESSAGE_ID, IPC_CREAT|0666);
+    while(1)
+	{
+		int returnValue = 0 ;
+		returnValue = msgrcv(msgID, &msgRx, sizeof(int), 0 ,0);
+			printf("EV_KEY(");
+			switch(msgRx.keyInput)
+			{
+                /*각 키가 눌렸을 때 행동 지정.*/
+				case KEY_VOLUMEUP: printf("Volume up key):"); break; 
+				case KEY_HOME: printf("Home key):"); break;
+				case KEY_SEARCH: printf("Search key):"); break;
+				case KEY_BACK: printf("Back key):"); break;
+				case KEY_MENU: printf("Menu key):"); break;
+				case KEY_VOLUMEDOWN:printf("Volume down key):");break;
+			}
+			printf("pressed\n");
+	}
+
 }
